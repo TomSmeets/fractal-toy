46cba3d6391131c1ae0132c89b33811581f8a19e
Tom Smeets <tom.tsmeets@gmail.com>
2020-03-13 09:57:27 +0100

    docker: improve build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -euf -o pipefail
 
+cd "$(dirname "$0")"
+
 function doit() {
+    echo "Building: $1"
     docker build -t "rust-build-$1" "./$1"
+    echo
+    echo
 }
 
 doit common
