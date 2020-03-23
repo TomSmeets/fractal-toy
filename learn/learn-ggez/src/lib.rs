9e5274e7e76364f5f0b210885ee85ddcb67f926e
Tom Smeets <tom.tsmeets@gmail.com>
2020-03-23 21:11:09 +0100

    learn/ggez: update

--- learn/learn-ggez/src/lib.rs
+++ learn/learn-ggez/src/lib.rs
@@ -3,17 +3,41 @@ use ggez::graphics;
 use ggez::{Context, GameResult};
 use std::result::Result::*;
 
+use ggez::conf::NumSamples;
 use ggez::graphics::*;
+
 use nalgebra as na;
 
 type P2 = na::Point2<f32>;
 type V2 = na::Vector2<f32>;
 
-pub struct MyGame {}
+pub struct MyGame {
+    canvas: Canvas,
+}
 
 impl MyGame {
-    pub fn new(ctx: &mut Context) -> MyGame {
-        MyGame {}
+    pub fn new(ctx: &mut Context) -> GameResult<MyGame> {
+        let size = 512;
+        let mut image_rgba = vec![255; size * size * 4];
+        for y in 0..size {
+            for x in 0..size {
+                image_rgba[(y * size + x) * 4 + 0] = 0;
+                image_rgba[(y * size + x) * 4 + 1] = 255;
+                image_rgba[(y * size + x) * 4 + 2] = 0;
+                image_rgba[(y * size + x) * 4 + 3] = 255;
+            }
+        }
+        let size = size as u16;
+        let img = Image::from_rgba8(ctx, size, size, &image_rgba)?;
+        let canvas: Canvas = Canvas::new(ctx, size, size, NumSamples::One)?;
+
+        // TODO: why is the scaling wrong in the image?
+        graphics::set_canvas(ctx, Some(&canvas));
+        graphics::clear(ctx, Color::from_rgba(255, 0, 255, 255));
+        graphics::draw(ctx, &img, DrawParam::default())?;
+        graphics::set_canvas(ctx, None);
+
+        Ok(MyGame { canvas })
     }
 }
 
@@ -24,22 +48,13 @@ impl EventHandler for MyGame {
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
         graphics::clear(ctx, graphics::WHITE);
-
-        graphics::draw(
-            ctx,
-            &Text::new("Hello world"),
-            DrawParam::default().color(Color::from_rgb(0, 0, 0)),
-        )?;
-
-        {
-            // Look at imgui-ggez-starter for texture creation. we need to create a gfx texture to be abel to update it
-            let pos = ggez::input::mouse::position(ctx);
-            let t = Text::new("How are you");
-            let p = DrawParam::default()
-                .color(Color::from_rgb(0, 0, 0))
-                .dest(pos);
-            graphics::draw(ctx, &t, p)?;
-        }
+        graphics::draw(ctx, &self.canvas, DrawParam::default()).unwrap();
         graphics::present(ctx)
+    }
+
+    // update canvas size on window reisze
+    fn resize_event(&mut self, ctx: &mut Context, width: f32, height: f32) {
+        let new_rect = graphics::Rect::new(0.0, 0.0, width as f32, height as f32);
+        graphics::set_screen_coordinates(ctx, new_rect).unwrap();
     }
 }
--- learn/learn-ggez/src/main.rs
+++ learn/learn-ggez/src/main.rs
@@ -1,3 +1,4 @@
+use ggez::conf::WindowMode;
 use ggez::event;
 use ggez::ContextBuilder;
 use std::result::Result::*;
@@ -12,14 +13,14 @@ use learn_ggez::MyGame;
 fn main() {
     // Make a Context and an EventLoop.
     let (mut ctx, mut event_loop) = ContextBuilder::new("game_name", "author_name")
-        .add_resource_path("resources")
+        .window_mode(WindowMode::default().resizable(true))
         .build()
         .unwrap();
 
     // Create an instance of your event handler.
     // Usually, you should provide it with the Context object
     // so it can load resources like images during setup.
-    let mut my_game = MyGame::new(&mut ctx);
+    let mut my_game = MyGame::new(&mut ctx).unwrap();
 
     // Run!
     match event::run(&mut ctx, &mut event_loop, &mut my_game) {
