3178aa4f15f72f680a4c17bf7fcedee732638a79
Tom Smeets <tom.tsmeets@gmail.com>
2020-03-25 11:30:06 +0100

    seriral: add dev binary

---
+++ serial/src/bin/dev.rs
@@ -0,0 +1,35 @@
+use serial::{
+    game::State,
+    module::input::InputAction,
+    state::{load, load_in_place, save},
+};
+
+fn main() {
+    let mut s = match load("auto") {
+        Ok(s) => s,
+        Err(_) => State::new(),
+    };
+
+    loop {
+        if s.update() {
+            break;
+        }
+
+        let do_save = s.input.button(InputAction::F5).went_down();
+        let do_load = s.input.button(InputAction::F6).went_down();
+
+       if do_save {
+           save("manual", &s);
+       }
+
+        if do_load {
+            let (s2, err) = load_in_place("manual", s);
+            if let Some(e) = err {
+                eprintln!("{}", e);
+            }
+            s = s2;
+        }
+    }
+
+    serial::state::save("auto", &s);
+}
--- serial/src/bin/linux.rs
+++ serial/src/bin/linux.rs
@@ -1,34 +1,9 @@
 use serial::{
     game::State,
-    module::input::InputAction,
-    state::{load, load_in_place, save},
 };
 
 fn main() {
-    let mut s = match load("auto") {
-        Ok(s) => s,
-        Err(_) => State::new(),
-    };
-    loop {
-        if s.update() {
-            break;
-        }
-
-        let do_save = s.input.button(InputAction::F5).went_down();
-        let do_load = s.input.button(InputAction::F6).went_down();
-
-        if do_save {
-            save("manual", &s);
-        }
-
-        if do_load {
-            let (s2, err) = load_in_place("manual", s);
-            if let Some(e) = err {
-                eprintln!("{}", e);
-            }
-            s = s2;
-        }
+    let mut s = State::new();
+    while !s.update() {
     }
-
-    serial::state::save("auto", &s);
 }
