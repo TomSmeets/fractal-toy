e699eecc2078a31e69f7d938562e26bf7dfedeb0
Tom Smeets <tom.tsmeets@gmail.com>
2020-03-27 17:31:09 +0100

    consider rcs

--- learn/learn-lifetime/src/main.rs
+++ learn/learn-lifetime/src/main.rs
@@ -1,27 +1,33 @@
+use std::rc::{Weak, Rc};
+
+#[derive(Debug)]
 struct Stuff {
-    strings: Vec<String>
+    current: Weak<String>,
+    strings: Vec<Rc<String>>,
 }
 
 impl Stuff {
-    pub fn new() -> Self { Stuff { strings: Vec::new() } }
+    pub fn new() -> Self {
+        let strings = vec![];
+        Stuff { strings, current: Weak::new() }
+    }
 
-    pub fn last(&self) -> Option<&str> {
-        Some(&self.strings[self.strings.len() - 1])
+    pub fn last(&self) -> Option<Rc<String>> {
+        self.current.upgrade()
     }
 
     pub fn push(&mut self, s: &str) {
-        self.strings.push(s.to_string());
+        let s = Rc::new(s.to_string());
+        self.current = Rc::downgrade(&s);
+        self.strings.push(s);
     }
 }
 
 fn main() {
     let mut stuff = Stuff::new();
-    let mut last = None;
-    println!("{:?}, {:?}", stuff.strings, last);
+    println!("{:?}, {:?}", stuff.strings, stuff.last());
     stuff.push("Hello");
-    last = stuff.last();
-    println!("{:?}, {:?}", stuff.strings, last);
+    println!("{:?}, {:?}", stuff.strings, stuff.last());
     stuff.push("World");
-    last = stuff.last();
-    println!("{:?}, {:?}", stuff.strings, last);
+    println!("{:?}, {:?}", stuff.strings, stuff.last());
 }
