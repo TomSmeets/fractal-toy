a53cbd5a2cc64d262793b219a35b323fa31f2c20
Tom Smeets <tom.tsmeets@gmail.com>
2020-03-28 22:25:53 +0100

    Some notes

--- maze/src/main.rs
+++ maze/src/main.rs
@@ -1,6 +1,10 @@
 mod backend;
 mod core;
 
+// core should not depend on any platform spesific stuff.
+// core is like a library. it should do as much as possible.
+// such that the platform spesific can be as minimal as possible
+
 fn main() {
     self::backend::run();
 }
