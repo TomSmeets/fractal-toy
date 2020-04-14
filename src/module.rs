f3c3b24174954f5c28581ceb4229a0947417ba57
Tom Smeets <tom.tsmeets@gmail.com>
2020-04-14 20:10:53 +0200

    Remove old ui module

--- src/module.rs
+++ src/module.rs
@@ -3,7 +3,6 @@ pub mod fractal;
 pub mod input;
 pub mod sdl;
 pub mod time;
-// pub mod ui;
 pub mod window;
 
 pub use self::{fractal::Fractal, input::Input, sdl::Sdl, time::Time, window::Window};
--- src/module/ui/drawcmd.rs
+++
@@ -1,7 +0,0 @@
-use crate::math::Rect;
-
-pub enum DrawCmd {
-    Rect(Rect, [f32; 4]),
-    Text(Rect, String),
-    Clip(Option<Rect>),
-}
