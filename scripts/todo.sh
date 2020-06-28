e1fa4253f894bfd0c111acb6579fea61ca6ebf65
Tom Smeets <tom.tsmeets@gmail.com>
2020-06-28 21:11:32 +0200

    scripts/todo: align columns

--- scripts/todo.sh
+++ scripts/todo.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-git ls-files | grep '.rs' | while read f; do git annotate -e "$f" | awk -v name="$f" '{ date=$3 " " $4; $1=$2=$3=$4=""; print date "\t" name "\t" $0 }'; done | grep 'TODO' | sort
+git ls-files | grep '.rs' | while read f; do git annotate -e "$f" | awk -F $'\t' -v name="$f" '{ date=$3 " " $4; $1=$2=$3=$4=$5=""; print date,name,$0 }'; done | grep 'TODO' | sort | column -t -s $'\t'
