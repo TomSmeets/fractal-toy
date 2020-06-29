cf51161edaf5edb574b538e4cd4b47356f8716b6
Tom Smeets <tom.tsmeets@gmail.com>
2020-06-29 14:09:34 +0200

    fractal/queue: PrioMutex init

--- fractal/src/fractal/queue.rs
+++ fractal/src/fractal/queue.rs
@@ -6,8 +6,44 @@ use crate::fractal::TileContent;
 use crate::fractal::Viewport;
 use crossbeam_channel::bounded;
 use crossbeam_channel::{Receiver, Sender};
-use std::sync::{Arc, Mutex};
+use std::sync::atomic::AtomicBool;
+use std::sync::atomic::Ordering;
+use std::sync::{Arc, Mutex, MutexGuard};
 use tilemap::TilePos;
+
+pub struct PrioMutex<T> {
+    master_lock: AtomicBool,
+    m: Mutex<T>,
+}
+
+impl<T> PrioMutex<T> {
+    pub fn new(data: T) -> Self {
+        PrioMutex {
+            master_lock: AtomicBool::new(false),
+            m: Mutex::new(data),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        loop {
+            if self.master_lock.load(Ordering::Relaxed) {
+                // wait a bit
+                std::thread::sleep(std::time::Duration::from_millis(20));
+                std::thread::yield_now();
+                continue;
+            }
+
+            return self.m.lock().unwrap();
+        }
+    }
+
+    pub fn lock_high(&self) -> MutexGuard<T> {
+        self.master_lock.store(true, Ordering::Relaxed);
+        let l = self.m.lock().unwrap();
+        self.master_lock.store(false, Ordering::Relaxed);
+        l
+    }
+}
 
 pub struct TaskMapWithParams {
     pub quit: bool,
@@ -23,7 +59,7 @@ pub struct TileResponse {
 }
 
 pub struct Queue {
-    pub tiles: Arc<Mutex<TaskMapWithParams>>,
+    pub tiles: Arc<PrioMutex<TaskMapWithParams>>,
     rx: Receiver<TileResponse>,
 
     // NOTE: clone to create handles, could be done differently
@@ -33,13 +69,13 @@ pub struct Queue {
 #[derive(Clone)]
 pub struct QueueHandle {
     tx: Sender<TileResponse>,
-    tiles: Arc<Mutex<TaskMapWithParams>>,
+    pub tiles: Arc<PrioMutex<TaskMapWithParams>>,
 }
 
 impl Queue {
     pub fn set_params(&mut self, p: &TileParams) {
         // update params
-        let mut m = self.tiles.lock().unwrap();
+        let mut m = self.tiles.lock_high();
         m.params = p.clone();
         m.params_version = m.params_version.wrapping_add(1);
         println!("set params {}", m.params_version);
@@ -50,7 +86,7 @@ impl Queue {
 
     pub fn update(&mut self, vp: &Viewport) -> usize {
         // update params
-        let mut m = self.tiles.lock().unwrap();
+        let mut m = self.tiles.lock_high();
 
         let new_iter = vp.get_pos_all();
         m.map.update_with(new_iter, |_, _| (), |_| Some(Task::Todo));
@@ -64,7 +100,7 @@ impl Queue {
         // The exact size does not matter much
         let (out_tx, out_rx) = bounded(64);
 
-        let tiles = Arc::new(Mutex::new(TaskMapWithParams {
+        let tiles = Arc::new(PrioMutex::new(TaskMapWithParams {
             quit: false,
             map: TaskMap::new(),
             params_version: 0,
@@ -89,7 +125,7 @@ impl Queue {
 
 impl Drop for Queue {
     fn drop(&mut self) {
-        self.tiles.lock().unwrap().quit = true;
+        self.tiles.lock_high().quit = true;
     }
 }
 
@@ -101,7 +137,7 @@ impl QueueHandle {
     }
 
     pub fn recv(&self) -> Result<Option<TileRequest>, ()> {
-        let mut ts = self.tiles.lock().unwrap();
+        let mut ts = self.tiles.lock();
 
         if ts.quit {
             return Err(());
