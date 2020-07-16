9a2b17cd9f8b54a7c6f4e6e6d1f35c07b1570ad6
Tom Smeets <tom.tsmeets@gmail.com>
2020-07-16 21:16:36 +0200

    Remove some old todos

--- fractal/src/fractal/builder.rs
+++ fractal/src/fractal/builder.rs
@@ -1,43 +1,26 @@
 //! A service that can produce fractal image tiles. This trait should only
 //! represent the method of computing and precision. Everything else is
 //! determined by the arguments to the generate function.
-//!
-//! TODO: This should eventually become a trait.
-//! Future implementations could be:
-//!
-//! TODO: switch between float/double depending on zoom level
-//! TODO: dynamically check float/double availiability on ocl/cuda
-//! 1) builders give a max depth and performance metric (but how do they check the params before
-//!    building a tile?, there is no peeking a mpmc channel)
-//!
-//! INFO:
-//!     cuda: cudaStreamQuery
-//!
-//! TODO: ablility to save and load multiple locations
-//! TODO: share saved location (via clipboard)
-//! TODO: when loading a location, we could transition to it, by first zooming out, then zooming in to that location
-//! TODO: share sets of locations?
-//!
-//! TODO: the user should be able to change the algorithm.
-//! This could be achieved with decent performance by doing each stage on
-//! all pixels at the same time. this way the use of sse instructions
-//! can be maximized and the number of comparisons minimized
-//! Tile size should probably be configurable by the generator backend
-//! implementations. As different backends have different optimal tile
-//! sizes. (this was attemted but it was not very fast at all, also it is very
-//! hard to use sse for this because each pixel in a tile needs a different iteration count)
-//!
-//! TODO: The colorscheme should be changable
-//! TODO: 1) don't return exact pixels, but the complex numbers and/or iterations, same stuff.
-//!       best would be to just return array of iteration count for each pixel. this way we also
-//!       don't need to rebuild the tiles when changing the colorscheme
-//!       also this would solve the horrible 'pixels -> implenetation spesific texture format'
-//!       trait/functions passed to Fractal
-//! TODO: 2) pass a colorscheme struct/trait via TileProperties. A colorscheme should be like a
-//!       function of (iter%1.0,  iter/max_iter) -> color, this cold be encoded as a lookup image
-//!
-//! Why 1: the 'fractal' part of the library becomes a lot simpler
-//! Why 2: possibly better performance
+
+// TODO: switch between float/double depending on zoom level
+// TODO: dynamically check float/double availiability on ocl/cuda
+// TODO: ablility to save and load multiple locations
+// TODO: share saved location (via clipboard)
+// TODO: when loading a location, we could transition to it, by first zooming out, then zooming in to that location
+// TODO: share sets of locations?
+// TODO: The user should be able to change the algorithm.
+//       This could be achieved with decent performance by doing each stage on
+//       all pixels at the same time. this way the use of sse instructions
+//       can be maximized and the number of comparisons minimized
+//       Tile size should probably be configurable by the generator backend
+//       implementations. As different backends have different optimal tile
+//       sizes. (this was attemted but it was not very fast at all, also it is very
+//       hard to use sse for this because each pixel in a tile needs a different iteration count)
+//       pass a colorscheme struct/trait via TileProperties. A colorscheme should be like a
+//       function of (iter%1.0,  iter/max_iter) -> color, this cold be encoded as a lookup image
+//
+// TODO: The colorscheme should be changable
+// TODO: The coloring method should be changable (orbit trap)
 
 pub mod cpu;
 
