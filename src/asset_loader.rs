8eb342cb5029de82f0bf3be716ea7002c6446a46
Tom Smeets <tom.tsmeets@gmail.com>
2021-07-04 11:39:09 +0200

    Retry parsing image if it failed

--- src/asset_loader.rs
+++ src/asset_loader.rs
@@ -20,11 +20,23 @@ impl AssetLoader {
             }
         }
 
-        let buf = ::image::open(path).unwrap().into_rgba8();
-        let (w, h) = buf.dimensions();
-        let data = buf.into_raw();
-        let i = Image::new(V2::new(w, h), data);
-        self.cache.insert(path.to_string(), (time, i.clone()));
-        i
+        loop {
+            let buf = ::image::open(path);
+            let buf = match buf {
+                Err(e) => {
+                    dbg!(e);
+                    std::thread::sleep_ms(100);
+                    continue;
+                },
+                Ok(buf) => buf,
+            };
+            let buf = buf.into_rgba8();
+
+            let (w, h) = buf.dimensions();
+            let data = buf.into_raw();
+            let i = Image::new(V2::new(w, h), data);
+            self.cache.insert(path.to_string(), (time, i.clone()));
+            break i;
+        }
     }
 }
