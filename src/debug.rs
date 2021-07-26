5fd6b9e882191af5155ed103e19e64ca494dc2e2
Tom Smeets <tom.tsmeets@gmail.com>
2021-07-26 12:34:26 +0200

    debug: add TreeStorage

--- src/debug.rs
+++ src/debug.rs
@@ -1,4 +1,6 @@
-use std::time::Instant;
+use std::{collections::BTreeMap, time::Instant};
+
+use instant::Duration;
 
 struct TimeEntry {
     dt_avrg: f32,
@@ -6,17 +8,75 @@ struct TimeEntry {
 }
 
 #[derive(Debug, Clone, Copy)]
-enum DebugEvent {
-    Push(&'static str, Instant),
-    Pop(Instant),
+struct DebugEvent {
+    start_time: Instant,
+    fame_duration:  Duration,
+}
+
+enum Item<T> {
+    Push(&'static str, T),
+    Pop,
+}
+
+pub struct TreeStorage<T> {
+    items: Vec<Item<T>>,
+}
+
+impl<T> TreeStorage<T> {
+    pub fn new() -> Self { TreeStorage { items: Vec::new() }}
+
+    pub fn restart(&mut self) {
+        // TODO: keep items
+        self.items.clear();
+    }
+
+    pub fn push(&mut self, name: &'static str, def: T) -> &mut T {
+        self.items.push(Item::Push(name, def));
+        match self.items.last_mut().unwrap() {
+            Item::Push(_, t) => t,
+            Item::Pop => unreachable!(),
+        }
+    }
+
+    pub fn pop(&mut self) -> &mut T {
+        let pop_ix = self.items.len();
+        self.items.push(Item::Pop);
+        let push_ix = Self::find_matching_push(&self.items, pop_ix);
+        match &mut self.items[push_ix] {
+            Item::Push(_, t) => t,
+            Item::Pop => unreachable!(),
+        }
+    }
+
+    fn find_matching_push(items: &[Item<T>], pop_ix: usize) -> usize {
+        let mut depth = 0;
+        let mut ix = pop_ix;
+
+        loop {
+            match items[ix] {
+                Item::Push(_, _) => depth -= 1,
+                Item::Pop        => depth += 1,
+            }
+
+            if depth == 0 {
+                return ix;
+            }
+
+            if ix == 0 {
+                panic!("OH NO");
+            }
+
+            ix = ix - 1;
+        }
+    }
 }
 
 pub struct Debug {
     info: String,
     out: String,
-
-    last_frame_events: Vec<DebugEvent>,
-    current_frame_events: Vec<DebugEvent>,
+    
+    last_frame_events: TreeStorage<DebugEvent>,
+    events: TreeStorage<DebugEvent>,
 }
 
 impl Debug {
@@ -25,65 +85,45 @@ impl Debug {
             info: String::new(),
             out: String::new(),
 
-            last_frame_events: Vec::new(),
-            current_frame_events: Vec::new(),
+            last_frame_events: TreeStorage::new(),
+            events: TreeStorage::new(),
         }
     }
 
     pub fn begin(&mut self) {
-        std::mem::swap(&mut self.current_frame_events, &mut self.last_frame_events);
-        self.current_frame_events.clear();
+        std::mem::swap(&mut self.events, &mut self.last_frame_events);
+        self.events.restart();
 
         std::mem::swap(&mut self.out, &mut self.info);
         self.info.clear();
     }
 
     pub fn push(&mut self, name: &'static str) {
-        self.current_frame_events
-            .push(DebugEvent::Push(name, Instant::now()));
+        let time = Instant::now();
+        let ev = self.events.push(name, DebugEvent {
+            start_time: time,
+            fame_duration: Duration::ZERO,
+        });
+        ev.start_time = time;
     }
 
     pub fn pop(&mut self) {
-        self.current_frame_events
-            .push(DebugEvent::Pop(Instant::now()));
+        let ev = self.events.pop();
+        let time = Instant::now();
+        ev.fame_duration = time - ev.start_time;
     }
 
     pub fn draw(&mut self) -> String {
         let mut result = std::mem::take(&mut self.out);
-
-        // 0 1 2 3 2 3 2 1 2 1 0
-        //  ( ( ( ) ( ) ) ( ) )
-        fn find_pop(ev: &[DebugEvent]) -> Instant {
-            let mut depth = 0;
-            for e in ev {
-                match e {
-                    DebugEvent::Push(_, _) => depth += 1,
-                    DebugEvent::Pop(t) => {
-                        depth -= 1;
-
-                        if depth == 0 {
-                            return *t;
-                        }
-                    }
-                }
-            }
-
-            panic!("Oh no")
-        }
-
         result.push_str("-----------\n");
 
         let mut depth = 0;
-        for i in 0..self.last_frame_events.len() {
-            let e = self.last_frame_events[i];
-
+        for e in self.last_frame_events.items.iter() {
             match e {
-                DebugEvent::Push(name, start_time) => {
-                    let end_time = find_pop(&self.last_frame_events[i..]);
-
+                Item::Push(name, ev) => {
                     result.push_str(&format!(
                         "{:6.2} µs | ",
-                        (end_time - start_time).as_micros()
+                        ev.fame_duration.as_micros()
                     ));
                     for _ in 0..depth {
                         result.push_str("  ");
@@ -94,7 +134,7 @@ impl Debug {
                     depth += 1;
                 }
 
-                DebugEvent::Pop(time) => {
+                Item::Pop => {
                     depth -= 1;
                 }
             }
